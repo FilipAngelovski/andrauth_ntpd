0543f0a666b807c64a0d6dd3a18d5cf2dc8d30c8
Filip Angelovski <f.angelovski88@gmail.com>
2018-02-26 16:26:31 +0100

    Initial commit

---
+++ app/src/main/java/NotesBuilder.java
@@ -0,0 +1,24 @@
+/**
+ * Created by filip on 26.2.18.
+ */
+
+public class NotesBuilder {
+    private String title,
+            content;
+
+    public NotesBuilder() {
+    }
+
+    public NotesBuilder(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
